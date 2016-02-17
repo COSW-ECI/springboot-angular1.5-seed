08f599f552b9ca838daac6a991819ddaeb7ec2d7
Hector <hector.cadavid@gmail.com>
2016-02-17 10:40:44 -0500

    springboot-agular-seed first commit

---
+++ src/main/java/edu/eci/cosw/AngulartestApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngulartestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngulartestApplication.class, args);
+	}
+}
